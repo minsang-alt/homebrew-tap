23582ba4861937d116da5dc5fc0f6968eb495cb9
김민상 (1팀 서버) <kms@dwhale.kr>
2026-03-07 12:45:40 +0900

    Update pasteclip 1.0.6 DMG sha256

--- Casks/pasteclip.rb
+++ Casks/pasteclip.rb
@@ -1,6 +1,6 @@
 cask "pasteclip" do
   version "1.0.6"
-  sha256 "7e357368997c2ba516b688378a944799621d6961123b3c78cb2c444510109d56"
+  sha256 "4c864b66ade4f246dce468038fef2afb75a1019f2947c1df8af67553bf777115"
 
   url "https://github.com/minsang-alt/PasteClip/releases/download/v#{version}/PasteClip-#{version}.dmg"
   name "PasteClip"
