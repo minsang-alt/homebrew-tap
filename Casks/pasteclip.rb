1d15e7ef76b25303f7118e8585c1001eb97980e9
김민상 (1팀 서버) <kms@dwhale.kr>
2026-04-03 09:26:45 +0900

    Update PasteClip to v1.0.9

--- Casks/pasteclip.rb
+++ Casks/pasteclip.rb
@@ -1,8 +1,8 @@
 cask "pasteclip" do
-  version "1.0.7"
-  sha256 "b7a037e8e4ac87751a6fdf1a4633182ab22f70d9910c42d8ac0733f7132fdd23"
+  version "1.0.9"
+  sha256 "70e5c23169509685dc1ac33fd2273d1bb2616e15ffb17d856cbfb73b4321f4d8"
 
-  url "https://github.com/minsang-alt/PasteClip/releases/download/v#{version}/PasteClip-#{version}.dmg"
+  url "https://github.com/minsang-alt/PasteClip/releases/download/v#{version}/PasteClip-v#{version}.dmg"
   name "PasteClip"
   desc "Lightweight clipboard manager for macOS"
   homepage "https://github.com/minsang-alt/PasteClip"
