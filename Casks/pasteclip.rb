a027988f22878e6f12102dec4d313d826b7486e2
김민상 (1팀 서버) <kms@dwhale.kr>
2026-04-08 00:02:08 +0900

    Update pasteclip to v1.1.7

--- Casks/pasteclip.rb
+++ Casks/pasteclip.rb
@@ -1,6 +1,6 @@
 cask "pasteclip" do
-  version "1.1.6"
-  sha256 "2d8412b02b85b0703d72152508015247600a7aa8e26464fb0167c03d41634f19"
+  version "1.1.7"
+  sha256 "6ab4f287268b23c23d62f123027f9afc235df9e7611d8a69c4aec774fb6a4ac1"
 
   url "https://github.com/minsang-alt/PasteClip/releases/download/v#{version}/PasteClip-v#{version}.dmg"
   name "PasteClip"
