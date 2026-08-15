027e2779ae399e5300e8adfa36e1127d35b81e00
Min <minseusang@gmail.com>
2026-08-15 18:12:43 +0900

    pasteclip 1.1.10

--- Casks/pasteclip.rb
+++ Casks/pasteclip.rb
@@ -1,25 +1,4 @@
 cask "pasteclip" do
-  version "1.1.9"
-  sha256 "7fcefd477dd704edd008f99d11996538693996bfad416ba344122de15223eda6"
-
-  url "https://github.com/minsang-alt/PasteClip/releases/download/v#{version}/PasteClip-v#{version}.dmg"
-  name "PasteClip"
-  desc "Lightweight clipboard manager for macOS"
-  homepage "https://github.com/minsang-alt/PasteClip"
-
-  depends_on macos: ">= :sonoma"
-
-  app "PasteClip.app"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-cr", "#{appdir}/PasteClip.app"]
-    system_command "/usr/bin/open",
-                   args: ["#{appdir}/PasteClip.app"]
-  end
-
-  zap trash: [
-    "~/Library/Application Support/PasteClip",
-    "~/Library/Preferences/com.minsang.PasteClip.plist",
+  vepÚ[ÛˆŒKŒKH‚ˆÚLMˆÙ˜ÙY™ÍÙÌYNYLNNMLÎLÎNM˜™˜YM˜˜LÍLŒ™LMLŒŒÙYMˆ‚‚ˆ\›šÎ‹ËÙÚ]X‹˜ÛÛKÛZ[œØ[™ËX[Ô\İPÛ\Ü™[X\Ù\ËÙİÛ›ØYİˆŞİ™\6–öçÒõ7FT6Æ—×b7·fW'6–öçÒæFÖr ¢æÖR%7FT6Æ— ¢FW62$Æ–v‡GvV–v‡B6Æ—&ö&BÖævW"f÷"Ö4õ2 ¢†öÖW”€‰¡ÑÑÁÌè¼½ithub.com/milØ[™ËX[Ô\İPÛ\‚‚ˆ\[™×ÛÛˆXXÛÜÎˆHœÛÛ›ÛXH‚‚ˆ\”\İPÛ\˜\‚‚ˆÜİ›YÚÂˆŞ\İ[WØÛÛ[X[™‹İ\Ü‹Øš[‹Ş]ˆ‹ˆ\™ÜÎˆÈ‹XÜˆ‹ˆŞØ\\ŸKÔ\İPÛ\˜\—BˆŞ\İ[WØÛÛ[X[™‹İ\Ü‹Øš[‹ÛÜ[ˆ‹ˆ\™ÜÎˆÈˆŞØ\\ŸKÔ\İPÛ\˜\—Bˆ[™‚ˆ˜\˜\ÚˆÂˆŸ‹ÓX&$½ÁÁ±¥…Ñ¥½¸MÕÁÁ½ÉĞ½A…ÍÑ•±¥Àˆ°(€€€€/Library/Preferences/com.minsang.PasteClip.plist",
   ]
 end
