311867aac15e37f20283e1c6a460d2338d8191cf
mobrava <82764703+mobrava@users.noreply.github.com>
2026-08-18 23:53:29 +0900

    cask URL을 새 계정명(mobrava)으로 업데이트

--- Casks/pasteclip.rb
+++ Casks/pasteclip.rb
@@ -2,10 +2,10 @@ cask "pasteclip" do
   version "1.1.11"
   sha256 "69ae94d7adc6b69aad52865e4ad3b38cd9ab97059d5221de254175c0f7d6ecce"
 
-  url "https://github.com/minsang-alt/PasteClip/releases/download/v#{version}/PasteClip-v#{version}.dmg"
+  url "https://github.com/mobrava/PasteClip/releases/download/v#{version}/PasteClip-v#{version}.dmg"
   name "PasteClip"
   desc "Lightweight clipboard manager for macOS"
-  homepage "https://github.com/minsang-alt/PasteClip"
+  homepage "https://github.com/mobrava/PasteClip"
 
   depends_on macos: ">= :sonoma"
 
