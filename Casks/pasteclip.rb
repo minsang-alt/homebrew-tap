9aded660e6b86c16ea7913a16b5126db84d6e784
Min <minseusang@gmail.com>
2026-08-20 22:52:19 +0900

    Deprecate pasteclip cask (renamed to clipbara)

--- Casks/pasteclip.rb
+++ Casks/pasteclip.rb
@@ -1,18 +1,17 @@
 cask "pasteclip" do
-  version "1.1.11"
-  sha256 "69ae94d7adc6b69aad52865e4ad3b38cd9ab97059d5221de254175c0f7d6ecce"
+  version "1.2.0"
+  sha256 "9fcb1ac2d071a722a65cd8334c185c79958f2a9cce3623929685fc973307eb48"
 
-  url "https://github.com/mobrava/PasteClip/releases/download/v#{version}/PasteClip-v#{version}.dmg"
-  name "PasteClip"
-  desc "Lightweight clipboard manager for macOS"
-  homepage "https://github.com/mobrava/PasteClip"
+  url "https://github.com/mobrava/Clipbara/releases/download/v#{version}/Clipbara-v#{version}.dmg"
+  name "PasteClip (renamed to Clipbara)"
+  desc "Renamed to Clipbara - install the clipbara cask instead"
+  homepage "https://github.com/mobrava/Clipbara"
+
+  deprecate! date: "2026-08-20", because: "it was renamed to clipbara"
 
   depends_on macos: ">= :sonoma"
 
-  app "PasteClip.app"
+  app "Clipbara.app"
 
-  zap trash: [
-    "~/Library/Application Support/PasteClip",
-    "~/Library/Preferences/com.minsang.PasteClip.plist",
-  ]
+  caveats "PasteClip has been renamed to Clipbara. Please install with:\n  brew install --cask mobrava/tap/clipbara"
 end
